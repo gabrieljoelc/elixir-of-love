521924617f73d4f3a6939daff61b8cff7f64ac1d
Gabriel Chaney <gabriel.chaney@gmail.com>
2017-05-22 09:46:19 -0500

    Add Heroku database config

--- config/prod.exs
+++ config/prod.exs
@@ -17,6 +17,13 @@ config :hello_world, HelloWorld.Endpoint,
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
+config :hello_world, HelloWorld.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: "hello_world",
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
 # Do not print debug messages in production
 config :logger, level: :info
 
